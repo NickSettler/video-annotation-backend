c60556379dee670e544bfec0da115f71f4c6ea46
NickSettler <moiseevnikita14@gmail.com>
2024-01-17 01:43:09 +0100

    feat(sentry): add sentry support

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM node:21-alpine as backend
 
+ARG VERSION
+ARG SENTRY_AUTH_TOKEN
+ENV SENTRY_AUTH_TOKEN ${SENTRY_AUTH_TOKEN}
+
 WORKDIR /app
 
 RUN apk --no-cache add --virtual builds-deps build-base python3 ffmpeg
@@ -16,14 +20,16 @@ COPY . .
 
 RUN yarn build
 
+RUN yarn run sentry-cli sourcemaps inject --org video-annotator --project video-annotator-backend -r $VERSION ./build && \
+    yarn run sentry-cli sourcemaps upload --org video-annotator --project video-annotator-backend -r $VERSION ./build
+
 EXPOSE 3000
 
 CMD ["sh", "-c", "yarn start:prod"]
 
 FROM postgres:16-alpine as database
 
+ADD ./docker/postgres/pg_hba.conf /var/lib/postgresql/data/
+ADD ./docker/postgres/postgresql.conf /var/lib/postgresql/data/
+
 COPY ./docker/postgres/migrations /docker-entrypoint-initdb.d
-
-EXPOSE 5432
-
-CMD ["postgres"]
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { config } from 'dotenv';
 import * as cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 import { ClassSerializerInterceptor } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as Sentry from '@sentry/node';
+import { ProfilingIntegration } from '@sentry/profiling-node';
+import pkg from '../package.json';
 
 async function bootstrap() {
   config();
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.use(cookieParser());
   app.use(helmet());
@@ -19,7 +23,28 @@ async function bootstrap() {
     origin: '*',
   });
 
+  const expressApp = app.getHttpAdapter().getInstance();
+
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    environment: process.env.NODE_ENV,
+    release: `${pkg.version}`,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Sentry.Integrations.Express({ app: expressApp }),
+      new ProfilingIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0,
+  });
+
+  app.use(Sentry.Handlers.requestHandler());
+
+  app.use(Sentry.Handlers.tracingHandler());
+
   await app.listen(3000);
+
+  app.use(Sentry.Handlers.errorHandler());
 }
 
 bootstrap();

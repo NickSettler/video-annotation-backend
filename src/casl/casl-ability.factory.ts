18d194ee3a915adff72ede990779fcc05292dd24
NickSettler <moiseevnikita14@gmail.com>
2024-02-09 01:51:35 +0100

    feat(auth): add access rules for video entity

--- src/casl/casl-ability.factory.ts
+++ src/casl/casl-ability.factory.ts
@@ -15,8 +15,11 @@ import {
   E_WORKSPACE_ENTITY_KEYS,
   Workspace,
 } from '../db/entities/workspace.entity';
+import { Video } from '../db/entities/video.entity';
 
-export type TSubjects = InferSubjects<typeof User | typeof Workspace> | 'all';
+export type TSubjects =
+  | InferSubjects<typeof User | typeof Video | typeof Workspace>
+  | 'all';
 
 export type TAbility = MongoAbility<
   [E_ACTION | typeof E_MANAGE_ACTION, TSubjects]
@@ -25,13 +28,23 @@ export type TAbility = MongoAbility<
 @Injectable()
 export class CaslAbilityFactory {
   private static applyUserRules(user: User, can: AddRule<TAbility>): void {
+    // User
     can([E_ACTION.READ, E_ACTION.UPDATE], User, {
       [E_USER_ENTITY_KEYS.ID]: user[E_USER_ENTITY_KEYS.ID],
     });
 
+    // Workspace
     can(E_ACTION.CREATE, Workspace);
 
     can([E_ACTION.READ, E_ACTION.UPDATE, E_ACTION.DELETE], Workspace, {
+      [`${[E_WORKSPACE_ENTITY_KEYS.CREATED_BY]}.${[E_USER_ENTITY_KEYS.ID]}`]:
+        user[E_USER_ENTITY_KEYS.ID],
+    });
+
+    // Video
+    can(E_ACTION.CREATE, Video);
+
+    can([E_ACTION.READ, E_ACTION.UPDATE, E_ACTION.DELETE], Video, {
       [`${[E_WORKSPACE_ENTITY_KEYS.CREATED_BY]}.${[E_USER_ENTITY_KEYS.ID]}`]:
         user[E_USER_ENTITY_KEYS.ID],
     });

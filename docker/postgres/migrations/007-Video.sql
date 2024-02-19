bb12270bdfe3f37a65df2a2648b29e6645c2031e
NickSettler <moiseevnikita14@gmail.com>
2024-02-19 22:38:23 +0100

    refactor(video): update video migration

--- docker/postgres/migrations/007-Video.sql
+++ docker/postgres/migrations/007-Video.sql
@@ -1,17 +1,19 @@
 DROP TABLE IF EXISTS video;
 
-CREATE TABLE video
+CREATE TABLE videos
 (
-    id         uuid    NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-    filename   varchar NOT NULL,
-    width      int     NOT NULL,
-    height     int     NOT NULL,
-    fps        float   NOT NULL,
-    bitrate    int     NOT NULL,
-    codec      varchar NOT NULL,
-    aspect_x   int     NOT NULL,
-    aspect_y   int     NOT NULL,
-    created_by uuid    NOT NULL,
-    created_at timestamp        DEFAULT now(),
+    id         uuid      NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+    name       varchar   NOT NULL,
+    filename   varchar,
+    poster_id  uuid,
+    width      int,
+    height     int,
+    fps        float,
+    bitrate    int,
+    codec      varchar,
+    aspect_x   int,
+    aspect_y   int,
+    created_by uuid      NOT NULL,
+    created_at timestamp NOT NULL DEFAULT now(),
     foreign key (created_by) references users (id) ON UPDATE CASCADE ON DELETE RESTRICT
 );

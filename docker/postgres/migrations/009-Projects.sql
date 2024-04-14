9cfdf9d895854ac9673b696163b34f5b29b1bd43
NickSettler <moiseevnikita14@gmail.com>
2024-04-14 14:11:28 +0200

    feat(db): add project migration

--- docker/postgres/migrations/009-Projects.sql
+++ docker/postgres/migrations/009-Projects.sql
@@ -2,11 +2,12 @@ DROP TABLE IF EXISTS projects;
 
 CREATE TABLE projects
 (
-    id         uuid      NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-    name       varchar   NOT NULL,
-    video_id   uuid      NOT NULL,
-    created_by uuid      NOT NULL,
-    created_at timestamp NOT NULL DEFAULT now(),
+    id          uuid      NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+    name        varchar   NOT NULL,
+    video_id    uuid      NOT NULL,
+    annotations jsonb     DEFAULT NULL,
+    created_by  uuid      NOT NULL,
+    created_at  timestamp NOT NULL DEFAULT now(),
     foreign key (video_id) references videos (id) ON UPDATE CASCADE ON DELETE RESTRICT,
     foreign key (created_by) references users (id) ON UPDATE CASCADE ON DELETE RESTRICT
 );
